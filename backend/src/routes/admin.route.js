878ade8f3a728d70945daa81c511df8385957713
pabhinav689@gmail.com <pabhinav689@gmail.com>
2026-02-18 12:51:16 +0530

    products page added

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProduct, getAllProducts, updateProduct, getAllOrders, updateOrderStatus, getAllCustomers, getDashboardStats } from "../controllers/admin.controller.js";
+import { createProduct, getAllProducts, updateProduct, getAllOrders, updateOrderStatus, getAllCustomers, getDashboardStats, deleteProduct } from "../controllers/admin.controller.js";
 
 import { protectRoute, adminOnly } from "../middleware/auth.middleware.js";
  import { upload } from "../middleware/multer.middleware.js";
@@ -14,6 +14,7 @@ router.use(protectRoute, adminOnly);
 router.post("/products", upload.array("images", 3), createProduct);
 router.get("/products", getAllProducts);
 router.put("/products/:id", upload.array("images", 3), updateProduct);
+router.delete("/products/:id", deleteProduct);   
 
 router.get("/orders", getAllOrders);
 router.patch("/orders/:orderId/status", updateOrderStatus);
